b770e5890985ec3b7284f5bdd948a58a6e9f5cfc
PeachBlossomWine <52470877+PeachBlossomWine@users.noreply.github.com>
2023-03-03 12:26:33 -0500

    Major change to handle auras
    Some unused code might remove later.

--- AutoItem.lua
+++ AutoItem.lua
@@ -1,7 +1,6 @@
-
 _addon.name = 'AutoItem'
-_addon.version = '3.6'
-_addon.author = 'Kate'
+_addon.version = '4.1'
+_addon.author = 'PBW'
 _addon.commands = {'autoitem','ai'}
 
 require('tables')
@@ -10,243 +9,86 @@ require('logger')
 require('sets')
 require 'lists'
 packets = require('packets')
-config = require('config')
 chat = require('chat')
 res = require('resources')
 
-
 active = true
-SJRestrict = false
-gaol_zones = S{279,298}
 job_registry = T{}
-defensedown = false
-defaults = {
-    remedy_buffs = S{},
-    panacea_buffs = S{},
-}
-settings = config.load(defaults)
+panacea_buffs = S{136,144,149,167}
+remedy_buffs = S{4}
+allbuffs = remedy_buffs:union(panacea_buffs)
+active_buffs = S{}
 
-allbuffs = {}
-n=0
-for k,v in pairs(settings.remedy_buffs) do table.insert(allbuffs,k) end
-for k,v in pairs(settings.panacea_buffs) do table.insert(allbuffs,k) end
-table.insert(allbuffs,"curse")
+local __bags = {}
+local getBagType = function(access, equippable)
+    return S(res.bags):filter(function(key) return (key.access == access and key.equippable == equippable and key.en ~= 'Recycle') or key.id == 0 and key end)
+end
 
-windower.register_event('gain buff', function(id)
-	zone_info = windower.ffxi.get_info()
-	local name = res.buffs[id].english
+do -- Setup Bags.
+    __bags.usable = T(getBagType('Everywhere', false))
+end
+
+local attempt = 0
+function use_meds_check()
+
+	if not active_buffs then return end
 	local player = windower.ffxi.get_player()
 
 	-- Remedy debuffs
-    for key,val in pairs(allbuffs) do
-		if val:lower() == name:lower() then
-            if settings.remedy_buffs:contains(name:lower()) and active and player.main_job ~= 'WHM' then
-				windower.add_to_chat(6,'[AutoItem] Gained remedy buff: ' .. name:lower() .. ' - ' .. id)
-                if haveMeds('Remedy') then
-                    while haveBuff(name:lower()) and active do
-                        windower.add_to_chat(6,"[AutoItem] Using Remedy.")
-                        windower.send_command('input /item "Remedy" <me>')
-                        coroutine.sleep(5.1)
-                    end
-                end
-            elseif settings.panacea_buffs:contains(name:lower()) and active and SJRestrict and gaol_zones:contains(zone_info.zone) then
-				windower.add_to_chat(6,'[AutoItem] Gained panacea buff: ' .. name:lower() .. ' - ' .. id)
-                if haveMeds('Panacea') then
-                    if defensedown == true and name:lower() == 'defense down' or name:lower() == 'magic def. down' then
-                        while haveBuff(name:lower()) and active do
-                            windower.add_to_chat(6,"[AutoItem] Using Panacea. - DEFENSE/MAGIC DEFENSE DOWN -")
-                            windower.send_command('input /item "Panacea" <me>')
-                            coroutine.sleep(5.1)
-                        end
-                    elseif name:lower() ~= 'defense down' then
-                        while haveBuff(name:lower()) and active do
-                            windower.add_to_chat(6,"[AutoItem] Using Panacea.")
-                            windower.send_command('input /item "Panacea" <me>')
-                            coroutine.sleep(5.1)
-                        end
-                    end
-                end
-            elseif name:lower() == 'curse' and active and player.main_job ~= 'WHM' then
-                if SJRestrict and gaol_zones:contains(zone_info.zone) and id == 20 then
-                    windower.add_to_chat(6,'[AutoItem] Gained sacrifice buff: ' .. name:lower() .. ' - ' .. id)
-                    while haveBuff("curse") and active do
-                        windower.add_to_chat(6,"[AutoItem] Sending WHM to Sacrifice: " .. find_job_charname('WHM'))
-                        windower.send_command('send '..find_job_charname('WHM')..' sacrifice '..windower.ffxi.get_player()["name"])
-                        coroutine.sleep(1.3)
-                    end	
-                elseif id == 9 then
-                    windower.add_to_chat(6,'[AutoItem] Gained debuff: ' .. name:lower() .. ' - ' .. id)
-                    if haveMeds('Holy Water') then
-                        while haveBuff("curse") and active do
-                            windower.add_to_chat(6,"[AutoItem] Using Holy Water.")
-                            windower.send_command('input /item "Holy Water" <me>')
-                            coroutine.sleep(5.1)
-                        end	
-                    end
-                end
-            end
-        end
-    end
-end)
-
-function haveMeds(medication)
-    local check_item_table = res.items:with('en',medication)
-    local check_item_id = check_item_table and check_item_table.id
-    local items = windower.ffxi.get_items()
-    local bags_to_check = L{'inventory','sack','case','satchel'}
-    for bag_name in bags_to_check:it() do
-        for index, item in pairs(items[bag_name]) do
-            if type(item) == 'table' and item.id == check_item_id then
-                windower.add_to_chat(262,"[AutoItem] F O U N D -> "..medication)
-                return true
-            end
-        end
-    end
+    for buff_id,_ in pairs (active_buffs) do
+		if remedy_buffs:contains(buff_id) and active and player.main_job ~= 'WHM' and (os.time()-attempt) > 4 then
+			if haveMeds(4155) and haveBuff(buff_id) then
+				windower.add_to_chat(6,"[AutoItem] Using Remedy.")
+				windower.send_command('input /item "Remedy" <me>')
+				attempt = os.time()
+			else
+				attempt = os.time()
+			end
+		elseif panacea_buffs:contains(buff_id) and active and (os.time()-attempt) > 4 then
+			if haveMeds(4149) and haveBuff(buff_id) then
+				windower.add_to_chat(6,"[AutoItem] Using Panacea.")
+				windower.send_command('input /item "Panacea" <me>')
+				attempt = os.time()
+			else
+				attempt = os.time()
+			end
+		elseif buff_id == 9 and active and player.main_job ~= 'WHM' and (os.time()-attempt) > 4 then
+            if haveMeds(4154) and haveBuff(buff_id) then
+				windower.add_to_chat(6,"[AutoItem] Using Holy Water.")
+				windower.send_command('input /item "Holy Water" <me>')
+				attempt = os.time()
+            else
+				attempt = os.time()
+			end
+		end
+	end
+	return
+end
 	
-	windower.add_to_chat(3, '[AutoItem] <<NO>> -' .. medication .. '- Found!')
+function haveMeds(med_id)
+	for bag in T(__bags.usable):it() do
+		for item, index in T(windower.ffxi.get_items(bag.id)):it() do
+			if type(item) == 'table' and item.id == med_id then
+				return true
+			end
+		end
+	end
+	
+	windower.add_to_chat(3, '[AutoItem] <<NO>> -' .. res.items[med_id].en .. '- Found!')
 	return false
 end
 
-function haveBuff(...)
-	local args = S{...}:map(string.lower)
+function haveBuff(buff_id)
 	local player = windower.ffxi.get_player()
-	if (player ~= nil) and (player.buffs ~= nil) then
+	if (player and player.buffs) then
 		for _,bid in pairs(player.buffs) do
-			local buff = res.buffs[bid]
-			if args:contains(buff.en:lower()) then
+			if buff_id == bid then
 				return true
 			end
 		end
 	end
 	return false
 end
-
-windower.register_event('addon command', function(...)
-    local args = {...}
-    if args[1] ~= nil then
-        local comm = args[1]:lower()
-        if comm == 'on' then
-            active = true
-			windower.add_to_chat(262,"[AutoItem] ON")
-        elseif comm == 'off' then
-			active = false
-            windower.add_to_chat(262,"[AutoItem] OFF")
-		elseif comm == 'dd' then
-			if not args[2] then
-				if defensedown then
-					defensedown = false
-					windower.add_to_chat(262,"[AutoItem] Defense Down INACTIVE!")
-				else
-					defensedown = true
-					windower.add_to_chat(262,"[AutoItem] Defense Down Activated!")
-				end
-			else
-				if args[2]:lower() == 'on' then
-					defensedown = true
-					windower.add_to_chat(262,"[AutoItem] Defense Down Activated!")
-				elseif args[2]:lower() == 'off' then
-					defensedown = false
-					windower.add_to_chat(262,"[AutoItem] Defense Down INACTIVE!")
-				end
-			end
-        elseif comm == 'check' then
-            table.remove(args, 1)
-            for k,v in pairs(args) do
-                args[k] = v:lower():ucfirst()
-            end
-            local arg_string = table.concat(args,' ')
-            windower.add_to_chat(262,"[AutoItem] Checking: "..arg_string)
-            haveMeds(arg_string)
-        elseif comm == 'test' then
-        local buffs = windower.ffxi.get_player()
-        for _,bid in pairs(buffs.buffs) do
-			--local buff = res.buffs[bid]
-			if bid == 259 then
-                table.vprint(buffs)
-			end
-		end
-        
-        end
-    end
-end)
-
-windower.register_event('load', function()
-
-	windower.add_to_chat(262,'[AutoItem] Welcome to AutoItem!')
-
-	zone_info = windower.ffxi.get_info()
-	if gaol_zones:contains(zone_info.zone) then
-		local current_buffs = windower.ffxi.get_player()["buffs"]
-		coroutine.sleep(5)
-		for key,val in pairs(current_buffs) do
-			if val == 157 then -- SJ Restriction
-				SJRestrict = true
-				windower.add_to_chat(262,'[AutoItem] Loaded in Sheol: Gaol')
-			end
-		end
-	end 
-end)
-
-windower.register_event('zone change', function(new_id, old_id)
-	zone_info = windower.ffxi.get_info()
-	coroutine.sleep(10)
-	if gaol_zones:contains(zone_info.zone) then
-		local current_buffs = windower.ffxi.get_player()["buffs"]
-
-		for key,val in pairs(current_buffs) do
-			if val == 157 then -- SJ Restriction
-				SJRestrict = true
-				windower.add_to_chat(262,'[AutoItem] Entered/zoned in Sheol: Gaol')
-			end
-		end
-	end 
-	
-	if gaol_zones:contains(old_id) and not gaol_zones:contains(new_id) then
-		windower.add_to_chat(262,'[AutoItem] Exiting Sheol: Gaol zones.')
-		SJRestrict = false
-	end
-end)
-
-windower.register_event('incoming chunk', function(id, data)
-    if id == 0x028 then	-- Casting
-        local action_message = packets.parse('incoming', data)
-		if action_message["Category"] == 4 then
-			isCasting = false
-		elseif action_message["Category"] == 8 then
-			isCasting = true
-		end
-	elseif id == 0x0DF then -- Char update
-        local packet = packets.parse('incoming', data)
-		if packet then
-			local playerId = packet['ID']
-			local job = packet['Main job']
-			
-			if playerId and playerId > 0 then
-				set_registry(packet['ID'], packet['Main job'])
-			end
-		end
-	elseif id == 0x0DD then -- Party member update
-        local packet = packets.parse('incoming', data)
-		if packet then
-			local playerId = packet['ID']
-			local job = packet['Main job']
-			
-			if playerId and playerId > 0 then
-				set_registry(packet['ID'], packet['Main job'])
-			end
-		end
-	elseif id == 0x0C8 then -- Alliance update
-        local packet = packets.parse('incoming', data)
-		if packet then
-			local playerId = packet['ID']
-			local job = packet['Main job']
-			
-			if playerId and playerId > 0 then
-				set_registry(packet['ID'], packet['Main job'])
-			end
-		end
-	end
-end)
 
 -- Credit to partyhints
 function set_registry(id, job_id)
@@ -287,3 +129,81 @@ function find_job_charname(job)
 	end
 	return 'NoJobFound'
 end
+
+local last_render = 0
+local delay = 0.5
+windower.register_event('prerender', function()
+
+  if (os.clock()-last_render) > delay then
+    use_meds_check()
+    last_render = os.clock()
+
+  end
+
+end)
+
+function handle_lose_buff(buff_id)
+	if buff_id and (remedy_buffs:contains(buff_id) or panacea_buffs:contains(buff_id)) then
+		active_buffs:remove(buff_id)
+		windower.add_to_chat(13,'[AutoItem] Debuff removed: ' .. res.buffs[buff_id].en .. ' - '..'['..buff_id..']')
+	end
+end	
+
+function handle_incoming_chunk(id, data)
+    if id == 0x028 then	-- Casting
+        local action_message = packets.parse('incoming', data)
+		if action_message["Category"] == 4 then
+			isCasting = false
+		elseif action_message["Category"] == 8 then
+			isCasting = true
+		end
+	elseif (id == 0x0DD or id == 0x0DF or id == 0x0C8) then	--Party member update
+        local parsed = packets.parse('incoming', data)
+		if parsed then
+			local playerId = parsed['ID']
+			local indexx = parsed['Index']
+			local job = parsed['Main job']
+			
+			if playerId and playerId > 0 then
+				set_registry(parsed['ID'], parsed['Main job'])
+			end
+		end
+	elseif id == 0x063 then -- Player buffs for Aura detection : Credit: elii, bp4
+		local parsed = packets.parse('incoming', data)
+		for i=1, 32 do
+			local buff = tonumber(parsed[string.format('Buffs %s', i)]) or 0
+			local time = tonumber(parsed[string.format('Time %s', i)]) or 0
+			
+			if buff > 0 and buff ~= 255 and (panacea_buffs:contains(buff) or remedy_buffs:contains(buff)) then
+				if not (math.ceil(1009810800 + (time / 60) + 0x100000000 / 60 * 9) - os.time() == 5) then
+					if not (active_buffs:contains(buff)) then
+						windower.add_to_chat(1, string.format("%s", ("[AutoItem] Debuff detected: %s - [%s]"):format(res.buffs[buff].en, buff):color(39)))
+						active_buffs:add(buff)
+					end
+				end
+			end
+		end
+	end
+end
+		
+function handle_addon(...)
+    local args = {...}
+    if args[1] ~= nil then
+        local comm = args[1]:lower()
+        if comm == 'on' then
+            active = true
+			windower.add_to_chat(262,"[AutoItem] ON")
+        elseif comm == 'off' then
+			active = false
+            windower.add_to_chat(262,"[AutoItem] OFF")
+	    end
+    end
+end
+
+windower.register_event('load', function()
+	windower.add_to_chat(262,'[AutoItem] Welcome to AutoItem!')
+end)
+
+windower.register_event('addon command',handle_addon)
+windower.register_event('lose buff', handle_lose_buff)
+windower.register_event('incoming chunk', handle_incoming_chunk)
