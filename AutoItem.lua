fc39d5d422dc1d103fb28448b231cd9139e44ca4
PeachBlossomWine <52470877+PeachBlossomWine@users.noreply.github.com>
2025-02-14 13:00:56 -0500

    Fix bugs

--- AutoItem.lua
+++ AutoItem.lua
@@ -19,9 +19,10 @@ job_registry = T{}
 panacea_buffs = S{13,136,144,145,146,149,167}	-- Slow, STR Down, Max HP Down, Max MP Down, Accuracy Down, Defense Down, Magic Def. Down
 dot_buffs = S{128,129,130,131,132,133} -- Burn, Frost, Choke, Rasp, Shock, Drown
 remedy_buffs = S{3,4,8}	-- Poison, Paralysis, Disease
+echo_buffs = S{6} -- Silence
 holywater_buffs = S{9}	-- Curse
 doom_buffs = S{15}
-allbuffs = remedy_buffs:union(panacea_buffs):union(holywater_buffs):union(dot_buffs):union(doom_buffs)
+allbuffs = remedy_buffs:union(panacea_buffs):union(holywater_buffs):union(dot_buffs):union(doom_buffs):union(echo_buffs)
 active_buffs = S{}
 
 local __bags = {}
@@ -41,7 +42,7 @@ function use_meds_check()
 
 	-- Remedy debuffs
     for buff_id,_ in pairs (active_buffs) do
-		if doom_buffs:contains(buff_id) and player.main_job ~= 'WHM' and (os.time()-attempt) > 4 then
+		if doom_buffs:contains(buff_id) and player.main_job ~= 'WHM' and (os.time()-attempt) > 1.5 then
             if haveBuff(buff_id) and haveMeds(4154) then
 				windower.add_to_chat(6,"[AutoItem] DOOMED - Using Holy Water.")
 				windower.send_command('input /item "Holy Water" <me>')
@@ -50,7 +51,16 @@ function use_meds_check()
 				active_buffs:remove(buff_id)
 				attempt = os.time()
 			end
-		elseif active and remedy_buffs:contains(buff_id) and (os.time()-attempt) > 4 then -- and player.main_job ~= 'WHM' 
+		elseif echo_buffs:contains(buff_id) and (os.time()-attempt) > 1.5 then
+            if haveBuff(buff_id) and haveMeds(4151) then
+				windower.add_to_chat(6,"[AutoItem] Silenced - Using Echo Drops.")
+				windower.send_command('input /item "Echo Drops" <me>')
+				attempt = os.time()
+            else
+				active_buffs:remove(buff_id)
+				attempt = os.time()
+			end
+		elseif active and remedy_buffs:contains(buff_id) and (os.time()-attempt) > 4 then
 			if haveBuff(buff_id) and haveMeds(4155) then
 				windower.add_to_chat(6,"[AutoItem] Using Remedy.")
 				windower.send_command('input /item "Remedy" <me>')
@@ -145,9 +155,11 @@ function handle_incoming_chunk(id, data)
 			local our_time = tonumber(parsed[string.format('Time %s', i)]) or 0
 			
 			if buff > 0 and buff ~= 255 and allbuffs:contains(buff) then
-				if math.ceil(1009810800 + (our_time / 60) + 0x100000000 / 60 * 10) - os.time() > 5 then
+				if buff == 15 or math.ceil(1009810800 + (our_time / 60) + 0x100000000 / 60 * 10) - os.time() > 5 then
 					if not (active_buffs:contains(buff)) then
-						if panacea and active and panacea_buffs:contains(buff) then
+						if doom_buffs:contains(buff) or echo_buffs:contains(buff) then
+							windower.add_to_chat(1, string.format("%s", ("[AutoItem] Debuff detected: %s - [%s]"):format(res.buffs[buff].en, buff):color(39)))
+						elseif panacea and active and panacea_buffs:contains(buff) then
 							windower.add_to_chat(1, string.format("%s", ("[AutoItem] Debuff detected: %s - [%s]"):format(res.buffs[buff].en, buff):color(39)))
 						elseif active and (remedy_buffs:contains(buff) or holywater_buffs:contains(buff)) then
 							windower.add_to_chat(1, string.format("%s", ("[AutoItem] Debuff detected: %s - [%s]"):format(res.buffs[buff].en, buff):color(39)))
